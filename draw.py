eb72b68a81b5df4e5c5cedb97562546732741285
Andrew Shao <ashao1@stuy.edu>
2019-02-10 23:54:30 -0500

    yes

--- draw.py
+++ draw.py
@@ -5,14 +5,45 @@ def draw_line( x0, y0, x1, y1, screen, color ):
     y = y0
     A = y1 - y0
     B = -(x1 - x0)
-    if A >= 0:
-        if B <= 0:
-            if -B > A:
+    if B <= 0:
+        if A >= 0:
+            if -B < A:
+                d = A + 2 * B
+                while y <= y1:
+                    plot(screen, color, x, y)
+                    if d < 0:
+                        x += 1
+                        d += 2 * A
+                    y += 1
+                    d += 2 * B
+            else:
                 d = 2 * A + B
                 while x <= x1:
                     plot(screen, color, x, y)
-                if d > 0:
-            y += 1
-            d += 2 * B
-        x += 1
-        d += 2 * A
+                    if d > 0:
+                        y += 1
+                        d += 2 * B
+                    x += 1
+                    d += 2 * A
+        else:
+            if B < A:
+                d = 2 * A + -B
+                while x <= x1:
+                    plot(screen, color, x, y)
+                    if d < 0:
+                        y -= 1
+                        d -= 2 * B
+                    x += 1
+                    d += 2 * A
+            else:
+                d = A + -2 * B
+                while y >= y1:
+                    plot(screen, color, x, y)
+                    if d < 0:
+                        x += 1
+                        d += 2 * A
+                    y -= 1
+                    d -= 2 * B
+    else:
+        draw_line(x1, y1, x0, y0, screen, color)
+                    
--- main.py
+++ main.py
@@ -4,9 +4,29 @@ from draw import *
 screen = new_screen()
 color = [ 0, 255, 0 ]
 
-draw_line(50, 50, 450, 450, screen, color)
-draw_line(50, 50, 450, 50, screen, color)
+
+# vertical
+draw_line(50, 50, 50, 450, screen, color)
+draw_line(450, 450, 450, 50, screen, color)
+
+
+draw_line(50, 50, 200, 350, screen, color)
+draw_line(300, 450, 150, 150, screen, color)
+draw_line(50, 100, 400, 450, screen, color)
+draw_line(450, 400, 100, 50, screen, color)
+draw_line(450, 300, 150, 150, screen, color)
 draw_line(50, 50, 350, 200, screen, color)
+
+# horizontal
+draw_line(50, 50, 450, 50, screen, color)
+draw_line(450, 450, 50, 450, screen, color)
+
+draw_line(50, 450, 350, 300, screen, color)
+draw_line(450, 50, 150, 200, screen, color)
+draw_line(100, 450, 450, 100, screen, color)
+draw_line(400, 50, 50, 400, screen, color)
+draw_line(50, 450, 300, 350, screen, color)
+draw_line(450, 50, 200, 150, screen, color)
 
 display(screen)
 save_extension(screen, 'img.png')
